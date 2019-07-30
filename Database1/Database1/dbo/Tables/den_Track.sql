3908b22723ed5266a3256a200d569195e09c6459
drebisz <darek369@vp.pl>
2019-07-30 18:33:05 +0200

    first

---
+++ Database1/Database1/dbo/Tables/den_Track.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[den_Track] (
+    [TrackId]       INT             NOT NULL,
+    [Name]          NVARCHAR (200)  NOT NULL,
+    [AlbumId]       INT             NULL,
+    [MediaTypeId]   INT             NOT NULL,
+    [GenreId]       INT             NULL,
+    [Composer]      NVARCHAR (220)  NULL,
+    [Milliseconds]  INT             NOT NULL,
+    [Bytes]         INT             NULL,
+    [UnitPrice]     NUMERIC (10, 2) NOT NULL,
+    [AlbumTitle]    NVARCHAR (160)  NOT NULL,
+    [ArtistName]    NVARCHAR (120)  NULL,
+    [MediaTypeName] NVARCHAR (120)  NULL,
+    [GenreName]     NVARCHAR (120)  NULL
+);
+
